b631046a46e5cbdf7983f3a72a28954256d4012c
MatsuuraKentaro <ceiiinosssttuv@gmail.com>
2017-01-10 18:28:24 +0900

    added as.matrix() because rstan 2.14 does not receive data.frame as data

--- chap08/run-model8-7.R
+++ chap08/run-model8-7.R
@@ -5,7 +5,7 @@ N <- nrow(d)
 Time <- c(1, 2, 4, 8, 12, 24)
 T_new <- 60
 Time_new <- seq(from=0, to=24, length=T_new)
-data <- list(N=N, T=length(Time), Time=Time, Y=d[,-1],
+data <- list(N=N, T=length(Time), Time=Time, Y=as.matrix(d[,-1]),
   T_new=T_new, Time_new=Time_new)
 fit <- stan(file='model/model8-7.stan', data=data, seed=1234)
 
