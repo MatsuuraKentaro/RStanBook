034fa6ef7a0c255f5415c553ae95230daafa64fa
ceiiinosssttuv@gmail.com <ceiiinosssttuv@gmail.com>
2021-12-30 22:56:25 +0900

    small update

--- chap05/exercise/ex5.R
+++ chap05/exercise/ex5.R
@@ -5,15 +5,14 @@ library(ggplot2)
 load('../output/result-model5-6.RData')
 ms <- rstan::extract(fit)
 
-d_qua <- t(apply(ms$m_pred, 2, quantile, prob=c(0.1, 0.5, 0.9)))
-colnames(d_qua) <- c('p10', 'p50', 'p90')
-d_qua <- data.frame(d, d_qua)
-d_qua$A <- as.factor(d_qua$A)
+qua <- apply(ms$m_pred, 2, quantile, prob=c(0.1, 0.5, 0.9))
+d_est <- data.frame(d, t(qua), check.names=FALSE)
+d_est$A <- as.factor(d_est$A)
 
-p <- ggplot(data=d_qua, aes(x=M, y=p50, ymin=p10, ymax=p90, shape=A, fill=A))
-p <- p + coord_fixed(ratio=1, xlim=c(10, 80), ylim=c(10, 80))
-p <- p + geom_pointrange(size=0.8)
-p <- p + geom_abline(aes(slope=1, intercept=0), color='black', alpha=3/5, linetype='31')
-p <- p + scale_shape_manual(values=c(21, 24))
-p <- p + labs(x='Observed', y='Predicted')
+p <- ggplot(data=d_est, aes(x=M, y=`50%`, ymin=`10%`, ymax=`90%`, shape=A, fill=A)) +
+  coord_fixed(ratio=1, xlim=c(10, 80), ylim=c(10, 80)) +
+  geom_pointrange(size=0.8) +
+  geom_abline(aes(slope=1, intercept=0), color='black', alpha=3/5, linetype='31') +
+  scale_shape_manual(values=c(21, 24)) +
+  labs(x='Observed', y='Predicted')
 ggsave(file='fig-ex5.png', plot=p, dpi=300, w=5, h=4)
