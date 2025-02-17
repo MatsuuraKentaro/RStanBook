63774eac66919be67a19504eac79d20de78d13c4
kn1kn1 <kn1kn1@users.noreply.github.com>
2025-02-17 19:49:47 +0900

    fix data list

--- chap08/run-model8-4.R
+++ chap08/run-model8-4.R
@@ -3,7 +3,7 @@ library(rstan)
 d <- read.csv('input/data-salary-2.txt')
 N <- nrow(d)
 K <- 4
-data <- list(N=N, X=d$X, Y=d$Y, KID=d$KID)
+data <- list(N=N, K=K, X=d$X, Y=d$Y, KID=d$KID)
 fit4 <- stan(file='model/model8-4.stan', data=data, seed=1234)
 
 save.image('output/result-model8-4.RData')
